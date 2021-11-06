2d3df5203015827b1f4464f70cc34d7df74d85ab
smilingprogrammer <abdulsoburoyewale@gmail.com>
2021-11-06 11:22:21 +0100

    App Modules Added

---
+++ app/src/main/java/com/example/weatherapp/api/AppModule.kt
@@ -0,0 +1,30 @@
+package com.example.weatherapp.api
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun getBaseUrl(): String = "https://api.openweathermap.org/data/2.5/"
+
+    @Provides
+    @Singleton
+    fun getRetrofitBuilder(baseUrl: String): Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun getApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+
+}
